38f2f36fe46a42856c36c7e9a215be5918da79d1
Chung Nguyen <zrmedia9@gmail.com>
2022-03-19 13:27:40 +0700

    Done project

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -17,5 +17,4 @@ export class AuthController {
   ): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialsDto);
   }
-
 }
--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,6 +1,7 @@
-import { User } from "src/auth/user.entity";
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
-import { TaskStatus } from "./task-status.enum";
+import { Exclude } from 'class-transformer';
+import { User } from 'src/auth/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { TaskStatus } from './task-status.enum';
 
 @Entity()
 export class Task {
@@ -13,6 +14,7 @@ export class Task {
   @Column()
   status: TaskStatus;
 
-  @ManyToOne(_type => User, user => user.tasks, { eager: false })
+  @ManyToOne((_type) => User, (user) => user.tasks, { eager: false })
+  @Exclude({ toPlainOnly: true })
   user: User;
 }
